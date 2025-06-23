c4caf43313dbd953eb61b477bde0aa30133ef6b2
ash-curtis <ashmcurtis1@gmail.com>
2025-06-23 17:12:15 +0100

    25.0623.01

--- opponent_pokemon_data.py
+++ opponent_pokemon_data.py
@@ -3,9 +3,9 @@ opponent_pokemon = {
   'name': 'Clodsire',
   'type': 'Grass',
   'moves': [
-    {'name': 'Poison Sting', 'power': 15, 'type': 'Poison'},
-    {'name': 'Stomp', 'power': 20, 'type': 'Normal'},
-    {'name': 'Water Spout', 'power': 20, 'type': 'Water'}
+    {'name': 'Poison Sting', 'power': 15, 'type': 'Poison', 'category': 'Physical'},
+    {'name': 'Stomp', 'power': 20, 'type': 'Normal', 'category': 'Physical'},
+    {'name': 'Water Spout', 'power': 20, 'type': 'Water', 'category': 'Special'}
   ],
   'status': None, # as not starting off with a condition
   'stats': {
